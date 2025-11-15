12c20f19a57b0f8a1bae20abb1319031a162acda
Sara <sara.salviano@escolar.ifrn.edu.br>
2025-11-15 02:24:36 +0000

    Read do dao feito 14/11

--- empresa/dao/base_dao.py
+++ empresa/dao/base_dao.py
@@ -20,24 +20,45 @@ class BaseDAO(ABC, Generic[T]):
     
   # Do formato JSON (dict) para o formato model (classe)
   @abstractmethod 
-  def to_model (self,data: dict) -> T: # Forçando o "seu filho" a implementar o método to_model e vai ter que retornar um tipo de dado
-    pass
+  def to_model(self, data: dict) -> T: # Forçando o "seu filho" a implementar o método to_model e vai ter que retornar um tipo de dado
+    raise NotImplementedError
   
   # Do modelo de dados (T) para o formato JSON (dict)
   @abstractmethod 
-  def to_dict (self, model: T) -> dict:
-    pass
+  def to_dict(self, model: T) -> dict:
+    raise NotImplementedError
   
   ### Create
+  def create(self, model: T) -> Optional[T]: #recebe um modelo genérico T
+    #Tratamento de erros
+    try:
+      data = self.to_dict(model) #conversão para dicionario
+      response = self.client.table(self.table_name).insert(data).execute() #comando para SUPABASE
+      if response.data:
+        return self.to_model(response.data[0])
+      return None #retorna
+    #Caso de errado
+    except Exception as e:
+      print(f"Erro ao criar registro: {e}") #mensagem de erro
+      return None #retorna nada
   
-  ### Read
-  
+  ### Read 
+  def read_id(self, record_id: int) -> Optional[T]:
+    try:
+      response = self.client.table(self.table_name).select('*').eq('id', record_id).execute()
+      if response.data:
+        return self.to_model(response.data[0])
+      return None
+    except Exception as e:
+      print(f'Erro ao buscar registro por id {record_id}: {e}')
+      return None
+    
   # Retorna todos os valores de uma tabela
   def read_all(self) -> List[T]:
     try:
-      response = self._client.table(self._table_name).select('*').execute()
+      response = self.client.table(self.table_name).select('*').execute()
       if response.data:
-        return[self.to_model(item) for item in response.data]
+        return [self.to_model(item) for item in response.data]
       return []
     except Exception as e:
       print(f"Erro ao buscar todos os registros: {e}")
@@ -45,7 +66,8 @@ class BaseDAO(ABC, Generic[T]):
   
   # Update
   
-  # delete
+  
+  # Delete
   
   
   
